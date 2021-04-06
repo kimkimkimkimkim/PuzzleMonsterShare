00139cbecd6fbc71af3ee8eebd63e087da015328
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-04-06 23:18:53 +0900

    MasterBookBaseの追加

---
+++ Data/Master/MasterBookBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// マスタデータの基本項目を定義するクラス
+/// </summary>
+public abstract class MasterBookBase
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    [Description("id")]
+    public long id { get; set; }
+}
