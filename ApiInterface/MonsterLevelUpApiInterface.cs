36e905db0f2fb294741194c26c20ebf866d94a26
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-04-12 00:42:36 +0900

    モンスター強化APIのインターフェース作成

---
+++ ApiInterface/MonsterLevelUpApiInterface.cs
@@ -0,0 +1,26 @@
+﻿public class MonsterLevelUpApiInterface
+{
+    public static string functionName = "MonsterLevelUp";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class MonsterLevelUpApiRequest
+{
+    /// <summary>
+    /// ユーザーモンスターID
+    /// </summary>
+    public long userMonsterId;
+
+    /// <summary>
+    /// 与える経験値量
+    /// </summary>
+    public int exp;
+}
+
+public class MonsterLevelUpApiResponse
+{
+    /// <summary>
+    /// 経験値付与後のレベル
+    /// </summary>
+    public int level;
+}
