0116225695a57e16a68a56900b0d48ec8380c90e
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-04-19 02:10:29 +0900

    クライアント・サーバー同期

---
+++ Const/ConstManager.cs
@@ -0,0 +1,29 @@
+﻿public class ConstManager
+{
+    public class System
+    {
+        /// <summary>
+        /// ユーザー名の最小文字数
+        /// </summary>
+        public static int userNameMinWordCount = 3;
+
+        /// <summary>
+        /// ユーザー名の最大文字数
+        /// </summary>
+        public static int userNameMaxWordCount = 10;
+    }
+
+    public class Monster
+    {
+        /// <summary>
+        /// 全モンスターの中での最大ステータス値
+        /// ゲージの最大値として使用する
+        /// </summary>
+        public static int MAX_STATUS_VALUE = 5000;
+
+        /// <summary>
+        /// 最大レベル
+        /// </summary>
+        public static int MAX_LEVEL = 100;
+    }
+}
--- Data/User/UserPropertyInfo.cs
+++ Data/User/UserPropertyInfo.cs
@@ -1,4 +1,7 @@
-﻿/// <summary>
+﻿using System.Collections.Generic;
+using System.Linq;
+using PM.Enum.Item;
+/// <summary>
 /// ユーザー資産情報
 /// </summary>
 public class UserPropertyInfo
@@ -17,4 +20,18 @@ public class UserPropertyInfo
     /// 数
     /// </summary>
     public int num { get; set; }
+}
+
+/// <summary>
+/// ユーザー資産情報の拡張クラス
+/// </summary>
+public static class UserPropertyExtensions
+{
+    /// <summary>
+    /// ユーザー資産リストから指定のタイプのユーザー資産情報を返す
+    /// </summary>
+    public static UserPropertyInfo GetOrDefault(this List<UserPropertyInfo> list, PropertyType type)
+    {
+        return list.FirstOrDefault(u => u.propertyId == (int)type);
+    }
 }
