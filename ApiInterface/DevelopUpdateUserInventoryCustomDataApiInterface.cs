c48a0ec576c4b910f6ab5219c184f2b57b037b92
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-04-22 21:53:25 +0900

    同期

---
+++ ApiInterface/DevelopUpdateUserInventoryCustomDataApiInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class DevelopUpdateUserInventoryCustomDataApiInterface
+{
+    public static string functionName = "DevelopUpdateUserInventoryCustomData";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class DevelopUpdateUserInventoryCustomDataApiRequest : PMApiRequestBase
+{
+    /// <summary>
+    /// PlayFabのアイテムインスタンスID
+    /// </summary>
+    public string itemInstanceId;
+
+    /// <summary>
+    /// 更新するカスタムデータ
+    /// </summary>
+    public Dictionary<string, string> data;
+}
+
+public class DevelopUpdateUserInventoryCustomDataApiResponse : PMApiResponseBase
+{
+
+}
