fc1bbec69cfdc883f70eb1fd44b141546bf95090
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-05-24 01:27:01 +0900

    同期

--- Enum/EnumManager.cs
+++ Enum/EnumManager.cs
@@ -187,6 +187,7 @@ namespace PM
                 userMonsterList,
                 userPropertyList,
                 userMonsterPartyList,
+                lastLoginDateTime,
             }
         }
 
---
+++ Util/DateTimeUtil.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class DateTimeUtil
+{
+    /// <summary>
+    /// 現在日時を返します
+    /// TODO : 海外などの対応
+    /// </summary>
+    public static DateTime Now(){
+        return DateTime.Now;
+    }
+}
