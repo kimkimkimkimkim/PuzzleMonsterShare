a30db2ba5a1c8a1a8ef8defcfa0f4378be3f86a1
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-05-25 22:13:14 +0900

    同期

---
+++ Data/Master/StaminaMB.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// スタミナマスタ
+/// </summary>
+[Description("StaminaMB")]
+public class StaminaMB : MasterBookBase
+{
+    /// <summary>
+    /// ランク
+    /// </summary>
+    public int rank { get; set; }
+
+    /// <summary>
+    /// スタミナ
+    /// </summary>
+    public int stamina { get; set; }
+}
---
+++ Data/Master/UserRankUpTableMB.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// ユーザーランク経験値テーブルマスタ
+/// </summary>
+[Description("UserRankUpTableMB")]
+public class UserRankUpTableMB : MasterBookBase
+{
+    /// <summary>
+    /// ランク
+    /// </summary>
+    public int rank { get; set; }
+
+    /// <summary>
+    /// １つ前のランクから目標ランクになるために必要な経験値量
+    /// </summary>
+    public int requiredExp { get; set; }
+
+    /// <summary>
+    /// 目標ランクになるために必要な総経験値量
+    /// </summary>
+    public int totalRequiredExp { get; set; }
+}
