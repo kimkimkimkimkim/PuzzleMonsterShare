1b606c84f7c3a416fd7eafefc37bb73de72bbd17
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-11-06 20:40:54 +0900

    状態異常情報も共有に

---
+++ Data/Battle/BattleConditionInfo.cs
@@ -0,0 +1,14 @@
+using PM.Enum.Battle;
+
+public class BattleConditionInfo
+{
+    /// <summary>
+    /// 状態異常タイプ
+    /// </summary>
+    public BattleConditionType type;
+    
+    /// <summary>
+    /// 残りターン数
+    /// </summary>
+    public int remainingTurnCount;
+}
