f4243e99d86cf003932892f6901378c3d6706d7a
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-11-17 08:59:31 +0900

    DateTimeUtilの修正

--- Util/DateTimeUtil.cs
+++ Util/DateTimeUtil.cs
@@ -6,7 +6,10 @@ public static class DateTimeUtil
     /// 現在日時を返します
     /// TODO : 海外などの対応
     /// </summary>
-    public static DateTime Now(){
-        return DateTime.Now;
-    }
+    public static DateTime Now => DateTime.Now;
+
+    /// <summary>
+    /// 初期値を返します
+    /// </summary>
+    public static DateTime Epoch => DateTime.MinValue;
 }
