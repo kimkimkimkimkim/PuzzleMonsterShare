e24aa5e3ee09bfbd4e024f23a38192a52def1492
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-11-19 08:46:14 +0900

    バトル報酬受け取りAPIのインターフェース作成

---
+++ ApiInterface/ReceiveBattleRewardApiInterface.cs
@@ -0,0 +1,21 @@
+﻿public class ReceiveBattleRewardApiInterface
+{
+    public static string functionName = "ReceiveBattleReward";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class ReceiveBattleRewardApiRequest : PMApiRequestBase
+{
+    /// <summary>
+    /// ユーザーバトル情報
+    /// </summary>
+    public UserBattleInfo userBattle;
+}
+
+public class ReceiveBattleRewardApiResponse : PMApiResponseBase
+{
+    /// <summary>
+    /// ユーザーバトル情報
+    /// </summary>
+    public UserBattleInfo userBattle;
+}
