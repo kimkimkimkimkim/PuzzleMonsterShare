b8d5c6b03bb6cf89fd9b18f79ce9d19d2a907c37
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-11-18 17:40:30 +0900

    Create ConditionMI.cs

---
+++ Data/Master/ConditionMI.cs
@@ -0,0 +1,17 @@
+using PM.Enum.Condition;
+
+/// <summary>
+/// あらゆる条件を扱うクラス
+/// </summary>
+public class ConditionMI
+{
+    /// <summary>
+    /// 条件タイプ
+    /// </summary>
+    public ConditionType type { get; set; }
+
+    /// <summary>
+    /// 値
+    /// </summary>
+    public int valueInt { get; set; }
+}
