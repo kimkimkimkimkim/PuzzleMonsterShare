8704dcb8bb4b88e2f31d8b1caec1047d62635834
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-12-12 12:01:32 +0900

    EndBattleApiInterfaceの作成

---
+++ ApiInterface/EndBattleApiInterface.cs
@@ -0,0 +1,26 @@
+﻿public class EndBattleApiInterface
+{
+    public static string functionName = "EndBattle";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class EndBattleApiRequest : PMApiRequestBase
+{
+    /// <summary>
+    /// ユーザーパーティID
+    /// </summary>
+    public string userMonsterPartyId;
+
+    /// <summary>
+    /// クエストID
+    /// </summary>
+    public long questId;
+}
+
+public class EndBattleApiResponse : PMApiResponseBase
+{
+    /// <summary>
+    /// ユーザーバトル情報
+    /// </summary>
+    public UserBattleInfo userBattle;
+}
