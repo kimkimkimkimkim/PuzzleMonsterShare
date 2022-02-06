fb9255658bf0380a498de96b03a70863be263b7d
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-02-07 01:14:56 +0900

    同期

--- Data/Error/PMApiException.cs
+++ Data/Error/PMApiException.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 public class PMApiException : Exception
 {
+    public PMApiException(SerializationInfo info, StreamingContext context)
+    {
+        if (info != null)
+            this.message = info.GetString("ErrorMessage");
+    }
+
     public PMErrorCode errorCode { get; set; }
     public string message { get; set; }
 }
