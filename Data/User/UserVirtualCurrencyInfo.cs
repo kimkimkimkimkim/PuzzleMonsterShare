f367ff5d0a4d461324e7aac30a46bfe040901ac1
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-02-07 01:15:06 +0900

    同期

--- Data/User/UserVirtualCurrencyInfo.cs
+++ Data/User/UserVirtualCurrencyInfo.cs
@@ -1,4 +1,7 @@
-﻿/// <summary>
+﻿using System.Collections.Generic;
+using PM.Enum.Item;
+
+/// <summary>
 /// ユーザー仮想通貨情報
 /// </summary>
 public class UserVirtualCurrencyInfo
@@ -22,4 +25,50 @@ public class UserVirtualCurrencyInfo
     /// モンスター経験値
     /// </summary>
     public int monsterExp { get; set; } = 0;
+
+    /// <summary>
+    /// 仮想通貨とその個数の情報リスト
+    /// </summary>
+    public List<VirtualCurrencyNumInfo> virtualCurrencyNumList {
+        get {
+            return new List<VirtualCurrencyNumInfo>() {
+                // オーブ
+                new VirtualCurrencyNumInfo() {
+                    virtualCurrencyId = (long)VirtualCurrencyType.OB,
+                    num = this.orb,
+                },
+                // コイン
+                new VirtualCurrencyNumInfo() {
+                    virtualCurrencyId = (long)VirtualCurrencyType.CN,
+                    num = this.coin,
+                },
+                // プレイヤー経験値
+                new VirtualCurrencyNumInfo() {
+                    virtualCurrencyId = (long)VirtualCurrencyType.PE,
+                    num = this.playerExp,
+                },
+                // モンスター経験値
+                new VirtualCurrencyNumInfo() {
+                    virtualCurrencyId = (long)VirtualCurrencyType.ME,
+                    num = this.monsterExp,
+                },
+            };
+        }
+    }
+
+}
+
+/// <summary>
+/// 仮想通貨とその個数の情報
+/// </summary>
+public class VirtualCurrencyNumInfo{
+    /// <summary>
+    /// 仮想通貨ID
+    /// </summary>
+    public long virtualCurrencyId { get; set; }
+
+    /// <summary>
+    /// 個数
+    /// </summary>
+    public int num { get; set; }
 }
