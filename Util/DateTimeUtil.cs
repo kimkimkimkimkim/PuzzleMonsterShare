df703a04cc669ca63614be202a37136d23658776
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-02-15 00:21:34 +0900

    DateTimeUtilの修正

--- Util/DateTimeUtil.cs
+++ Util/DateTimeUtil.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using PM.Enum.Mission;
 
 public static class DateTimeUtil
 {
@@ -42,16 +43,40 @@ public static class DateTimeUtil
     /// </summary>
     public static bool IsToday(DateTime date)
     {
-        // 現在時刻とゲーム内の今日が同じ日にちか否か
-        var isSameDate = Now.Hour >= ConstManager.System.START_DAY_HOUR;
+        var startAndEndDate = GetStartAndEndDate(MissionType.Daily);
 
-        // 現在時刻とゲーム内の今日が同じ日にちなら基準日は現在時刻、違うなら基準日は現在時刻の一日前
-        var baseDate = isSameDate ? Now : Now.AddDays(-1);
+        // 終了日時は開始日時と被らないようになっているのでどちらもイコールをつける
+        return startAndEndDate.startDate <= date && date <= startAndEndDate.endDate;
+    }
 
-        var startDate = new DateTime(baseDate.Year, baseDate.Month, baseDate.Day, ConstManager.System.START_DAY_HOUR, 0, 0);
-        var endDate = startDate;
-        endDate.AddDays(1);
+    /// <summary>
+    /// 指定したミッションタイプに応じた開始日時、終了日時を返す
+    /// </summary>
+    public static (DateTime startDate, DateTime endDate) GetStartAndEndDate(MissionType missionType){
+        switch(missionType){
+            case MissionType.Daily:
+                var endDayHour = ConstManager.System.START_DAY_HOUR - 1;
+                if(endDayHour < 0) endDayHour += 24;
 
-        return startDate <= date && date < endDate;
+                // 現在時刻とゲーム内の今日が同じ日にちか否か
+                var isSameDate = Now.Hour >= ConstManager.System.START_DAY_HOUR;
+
+                // 現在時刻とゲーム内の今日が同じ日にちなら基準日は現在時刻、違うなら基準日は現在時刻の一日前
+                var baseDate = isSameDate ? Now : Now.AddDays(-1);
+
+                var startDate = new DateTime(baseDate.Year, baseDate.Month, baseDate.Day, ConstManager.System.START_DAY_HOUR, 0, 0);
+                var endDate = startDate;
+                endDate.AddDays(1);
+                endDate = new DateTime(endDate.Year, endDate.Month, endDate.Day, endDayHour, 59, 59, 999);
+
+                return (startDate, endDate);
+            // Weekly, Monthlyの実装
+            case MissionType.Weekly:
+            case MissionType.Monthly:
+            case MissionType.Main:
+            case MissionType.Event:
+            default:
+                return (Epoch, Epoch);
+        }
     }
 }
