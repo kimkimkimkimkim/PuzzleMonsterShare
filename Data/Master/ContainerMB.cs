962ecbe590138285d8f73029603e34071bc1afbb
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-02-16 01:12:03 +0900

    コンテナマスタの作成

---
+++ Data/Master/ContainerMB.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+/// <summary>
+/// コンテナマスタ
+/// </summary>
+[Description("BundleMB")]
+public class ContainerMB : MasterBookBase
+{
+    /// <summary>
+    /// クエストカテゴリ名
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// アイテムリスト
+    /// </summary>
+    public List<ItemMI> itemList { get; set; }
+}
