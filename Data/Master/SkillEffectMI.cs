6f014c9af40af59a22da98d6d711441823eb645f
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-02 12:33:03 +0900

    スキル効果MIのアクション系状態異常への対応

--- Data/Master/SkillEffectMI.cs
+++ Data/Master/SkillEffectMI.cs
@@ -6,29 +6,34 @@
 public class SkillEffectMI
 {
     /// <summary>
-    /// スキル対象タイプ
+    /// スキルタイプ（共通）
+    /// </summary>
+    public SkillType type { get; set; }
+    
+    /// <summary>
+    /// スキル対象タイプ（共通）
     /// </summary>
     public SkillTargetType skillTargetType { get; set; }
 
     /// <summary>
-    /// スキル対象に対する発動条件タイプ
+    /// スキル対象に対する発動条件タイプ（共通）
     /// </summary>
     public ActivateConditionType activateConditionType { get; set; }
 
     /// <summary>
-    /// 効果量の対象タイプ
+    /// 効果量の対象タイプ（攻撃、回復、アクション系以外の状態異常用）
     /// </summary>
     public ValueTargetType valueTargetType { get; set; }
 
     /// <summary>
-    /// スキルタイプ
-    /// </summary>
-    public SkillType type { get; set; }
-
-    /// <summary>
-    /// 効果量（%）
+    /// 効果量（%）（攻撃、回復、アクション系以外の状態異常用）
     /// </summary>
     public int value { get; set; }
+    
+    /// <summary>
+    /// スキル演出ID（攻撃、回復、アクション系以外の状態異常用）
+    /// </summary>
+    public long skillFxId { get; set; }
 
     /// <summary>
     /// 状態異常タイプ（状態異常用）
@@ -39,9 +44,14 @@ public class SkillEffectMI
     /// 継続ターン数（状態異常用）
     /// </summary>
     public int durationTurnNum { get; set; }
-
+    
     /// <summary>
-    /// スキル演出ID
+    /// 解除可能か否か（状態異常用）
     /// </summary>
-    public long skillFxId { get; set; }
+    public bool canUnlock { get; set; }
+    
+    /// <summary>
+    /// 参照する状態異常効果リストの番号（アクション系の状態異常用）
+    /// </summary>
+    public int actionBattleConditionEffectListNumber { get; set; }
 }
