727f3f667cd35952e60b7bd164297c8715876e14
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-02 12:36:56 +0900

    ActionBattleConditionEffectMIの作成

---
+++ Data/Master/ActionBattleConditionEffectMI.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// アクション系の状態異常効果を扱うためのクラス
+/// </summary>
+public class ActionBattleConditionEffectMI: SkillEffectMI
+{
+    /// <summary>
+    /// スキルトリガータイプ
+    /// </summary>
+    public SkillTriggerType triggerType { get; set; }
+}
