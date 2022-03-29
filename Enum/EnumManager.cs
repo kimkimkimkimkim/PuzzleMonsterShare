4982a533b854a704a131189be60c0c6daad21906
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-30 01:44:56 +0900

    エラー解消

--- Enum/EnumManager.cs
+++ Enum/EnumManager.cs
@@ -1001,6 +1001,7 @@ namespace PM
             /// </summary>
             public enum PropertyType
             {
+                None = 0,
             }
 
             /// <summary>
@@ -1163,28 +1164,6 @@ namespace PM
                 Green = 3,
                 Yellow = 4,
                 Purple = 5,
-            }
-
-            public static class MonsterAttributeExtends
-            {
-                public static Color Color(this MonsterAttribute attribute)
-                {
-                    switch (attribute)
-                    {
-                        case MonsterAttribute.Red:
-                            return new Color(0.1529412f, 0.03921569f, 0.03137255f);
-                        case MonsterAttribute.Blue:
-                            return new Color(0.007843138f, 0.07450981f, 0.2039216f);
-                        case MonsterAttribute.Green:
-                            return new Color(0.07843138f, 0.1803922f, 0.1333333f);
-                        case MonsterAttribute.Yellow:
-                            return new Color(0.9647059f, 0.8823529f, 0.6117647f);
-                        case MonsterAttribute.Purple:
-                            return new Color(0.1294118f, 0.05098039f, 0.2039216f);
-                        default:
-                            return new Color(0, 0, 0);
-                    }
-                }
             }
         }
         
---
+++ Extensions/UserDataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using PM.Enum.Item;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class UserDataExtensions
+{
+    public static UserPropertyInfo GetOrDefault(this List<UserPropertyInfo> userPropertyList, PropertyType propertyType)
+    {
+        return userPropertyList.FirstOrDefault(u => (PropertyType)u.propertyId == propertyType);
+    }
+
+    public static int GetNum(this List<UserPropertyInfo> userPropertyList, PropertyType propertyType)
+    {
+        return userPropertyList.FirstOrDefault(u => (PropertyType)u.propertyId == propertyType)?.num ?? 0;
+    }
+}
