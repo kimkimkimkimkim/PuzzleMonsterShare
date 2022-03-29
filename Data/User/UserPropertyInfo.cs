d1a7234e4305b1e6c4e69126382db58a8c2c997a
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-30 01:48:14 +0900

    エラー解消

--- Data/User/UserPropertyInfo.cs
+++ Data/User/UserPropertyInfo.cs
@@ -16,26 +16,3 @@ public class UserPropertyInfo: InventoryDataBase
     /// </summary>
     public int num { get; set; }
 }
-
-/// <summary>
-/// ユーザー資産情報の拡張クラス
-/// </summary>
-public static class UserPropertyExtensions
-{
-    /// <summary>
-    /// ユーザー資産リストから指定のタイプのユーザー資産情報を返す
-    /// </summary>
-    public static UserPropertyInfo GetOrDefault(this List<UserPropertyInfo> list, PropertyType type)
-    {
-        return list.FirstOrDefault(u => u.propertyId == (int)type);
-    }
-
-    /// <summary>
-    /// 指定したユーザー資産の所持数を返す
-    /// </summary>
-    public static int GetNum(this List<UserPropertyInfo> list, PropertyType type)
-    {
-        var userProperty = list.GetOrDefault(type);
-        return userProperty == null ? 0 : userProperty.num;
-    }
-}
