89a74808d37f99e28de6e45203a5e70393855ba1
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-04-08 17:04:41 +0900

    QuestMonsterMIの作成

---
+++ Data/Master/QuestMonsterMI.cs
@@ -0,0 +1,11 @@
+public class QuestMonsterMI {
+    /// <summary>
+    /// モンスターID
+    /// </summary>
+    public long monsterId { get; set; }
+    
+    /// <summary>
+    /// モンスターID
+    /// </summary>
+    public int level { get; set; }
+}
