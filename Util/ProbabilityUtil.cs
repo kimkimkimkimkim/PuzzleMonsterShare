907e44d1fd08d393f82a778ef0b1f750fb034f8b
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-04-10 13:21:09 +0900

    ProbabilityUtilの作成

---
+++ Util/ProbabilityUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// 確率関係を扱うUtil
+/// </summary>
+public static class ProbabilityUtil
+{
+    /// <summary>
+    /// 指定した確率で当選したか否かを返す
+    /// </summary>
+    /// <param name="percent">0～100</param>
+    public static bool DetectByPercent(double percent)
+    {
+        var random = new Random(Guid.NewGuid().GetHashCode()).NextDouble() * 100;
+        return percent <= random;
+    }
+}
