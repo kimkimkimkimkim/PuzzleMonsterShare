3812f0c65b7e878adf29f180bb93b118e564771b
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-04-19 22:54:01 +0900

    GachaRewardItemMIの修正

--- Data/Master/GachaRewardItemMI.cs
+++ Data/Master/GachaRewardItemMI.cs
@@ -2,5 +2,16 @@
 /// ガチャ報酬アイテム情報
 /// </summary>
 public class GachaRewardItemMI : ItemMI
-{
+{   
+    /// <summary>
+    /// 確定枠か否か
+    /// </summary>
+    public bool isConfirmed { get; set; }
+
+    public GachaRewardItemMI(ItemMI item)
+    {
+        itemType = item.itemType;
+        itemId = item.itemId;
+        num = item.num;
+    }
 }
