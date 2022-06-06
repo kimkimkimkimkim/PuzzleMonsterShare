e3ae3d7bba363ba83c0c20d69134535d9d827c60
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-06-06 22:53:46 +0900

    イベントスケジュールマスタの追加

---
+++ Data/Master/EventQuestScheduleMB.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// イベントクエストスケジュールマスタ
+/// </summary>
+[Description("EventQuestScheduleMB")]
+public class EventQuestScheduleMB : MasterBookBase
+{
+    /// <summary>
+    /// 説明
+    /// </summary>
+    public string description { get; set; }
+
+    /// <summary>
+    /// 対象のクエストカテゴリID
+    /// </summary>
+    public long questCategoryId { get; set; }
+
+    /// <summary>
+    /// 開始日時（XXXX-XX-XX XX:XX:XX）
+    /// </summary>
+    public string startDate { get; set; }
+
+    /// <summary>
+    /// 終了日時（XXXX-XX-XX XX:XX:XX）
+    /// </summary>
+    public string endDate { get; set; }
+}
