bec65a758a1f7a8fff31f1d83112c06f029790ca
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-06-08 23:25:22 +0900

    デシリアライズできないのでコンストラクタを削除

--- Data/Master/GachaRewardItemMI.cs
+++ Data/Master/GachaRewardItemMI.cs
@@ -12,11 +12,4 @@ public class GachaRewardItemMI : ItemMI
     /// 確定枠か否か
     /// </summary>
     public bool isConfirmed { get; set; }
-
-    public GachaRewardItemMI(ItemMI item)
-    {
-        itemType = item.itemType;
-        itemId = item.itemId;
-        num = item.num;
-    }
 }
