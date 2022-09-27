7a62cbdd9c29548e22a137c3860071fe948b910a
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-09-27 21:36:49 +0900

    全資産付与APIの作成

---
+++ ApiInterface/DevelopGrantAllPropertyApiInterface.cs
@@ -0,0 +1,12 @@
+﻿public class DevelopGrantAllPropertyApiInterface
+{
+    public static string functionName = "DevelopGrantAllProperty";
+}
+
+public class DevelopGrantAllPropertyApiRequest : PMApiRequestBase
+{
+}
+
+public class DevelopGrantAllPropertyApiResponse : PMApiResponseBase
+{
+}
