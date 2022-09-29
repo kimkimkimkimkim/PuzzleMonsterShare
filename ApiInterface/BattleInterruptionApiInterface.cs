7f0a13442d83df8bbca86967591529c11e8b5f40
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-09-30 07:51:41 +0900

    バトル中断ApiInterfaceの作成

---
+++ ApiInterface/BattleInterruptionApiInterface.cs
@@ -0,0 +1,17 @@
+﻿public class BattleInterruptionApiInterface
+{
+    public static string functionName = "BattleInterruption";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class BattleInterruptionApiRequest : PMApiRequestBase
+{
+    /// <summary>
+    /// ユーザーバトルID
+    /// </summary>
+    public string userBattleId;
+}
+
+public class BattleInterruptionApiResponse : PMApiResponseBase
+{
+}
