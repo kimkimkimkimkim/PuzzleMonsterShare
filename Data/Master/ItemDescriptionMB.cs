41d2de08d2941f95f476e44a90a894f03de34be5
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-10-26 11:54:02 +0900

    アイテム説明文マスタの作成

---
+++ Data/Master/ItemDescriptionMB.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using PM.Enum.Item;
+
+/// <summary>
+/// アイテム説明文マスタ
+/// </summary>
+[Description("ItemDescriptionMB")]
+public class ItemDescriptionMB : MasterBookBase {
+    /// <summary>
+    /// アイテムタイプ
+    /// </summary>
+    public ItemType itemType { get; set; }
+
+    /// <summary>
+    /// アイテムID
+    /// </summary>
+    public long itemId { get; set; }
+
+    /// <summary>
+    /// 説明文
+    /// </summary>
+    public string description { get; set; }
+}
