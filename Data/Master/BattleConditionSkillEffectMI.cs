8a3dd969f374360b929a6cc5cbc71d979f16ae46
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-01-06 16:27:16 +0900

    battleConditionMBにskillEffectを追加

---
+++ Data/Master/BattleConditionSkillEffectMI.cs
@@ -0,0 +1,28 @@
+﻿using PM.Enum.Battle;
+
+/// <summary>
+/// 状態異常スキル効果を扱うためのクラス
+/// </summary>
+public class BattleConditionSkillEfectMI : SkillEffectMI {
+    /// <summary>
+    /// 発動制限回数タイプ
+    /// </summary>
+    public SkillExecuteNumLimitType executeNumLimitType { get; set; }
+
+    /// <summary>
+    /// 発動制限回数
+    /// 0 は制限なし
+    /// </summary>
+    public int limitExecuteNum { get; set; }
+
+    /// <summary>
+    /// スキルトリガータイプ
+    /// </summary>
+    public SkillTriggerType triggerType { get; set; }
+
+    /// <summary>
+    /// トリガータイプ用のオプション値
+    /// 状態異常付与トリガーの状態異常IDなど
+    /// </summary>
+    public int triggerTypeOptionValue { get; set; }
+}
