f1b9ec627132f6a20b984d848547d6b6e7bd51e0
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-04-21 17:52:09 +0900

    スタミナ回復薬使用APIインターフェースの作成

---
+++ ApiInterface/UseStaminaRecoveryApiInterface.cs
@@ -0,0 +1,14 @@
+﻿public class UseStaminaRecoveryApiInterface {
+    public static string functionName = "UseStaminaRecovery";
+}
+
+// APIリクエストの変数はプロパティにしてはいけない！(シリアライズの関係)
+public class UseStaminaRecoveryApiRequest : PMApiRequestBase {
+    /// <summary>
+    /// スタミナ回復薬の使用個数
+    /// </summary>
+    public int num;
+}
+
+public class UseStaminaRecoveryApiResponse : PMApiResponseBase {
+}
