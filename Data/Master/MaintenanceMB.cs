a59b4db31f545f0a41f5659437d8eedaa24f9e60
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-04-26 02:46:33 +0900

    メンテナンスMBの作成

---
+++ Data/Master/MaintenanceMB.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// メンテナンスマスタ
+/// </summary>
+[Description("MaintenanceMB")]
+public class MaintenanceMB : MasterBookBase
+{
+    /// <summary>
+    /// 開始日時（XXXX-XX-XX XX:XX:XX）
+    /// </summary>
+    public string startDate { get; set; }
+
+    /// <summary>
+    /// 終了日時（XXXX-XX-XX XX:XX:XX）
+    /// </summary>
+    public string endDate { get; set; }
+
+    /// <summary>
+    /// 本文
+    /// nullなら定型文
+    /// </summary>
+    public string text { get; set; }
+}
