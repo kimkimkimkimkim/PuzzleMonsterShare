7b477ace107b0bcbe8f0836a45b9e85008e5d1fe
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-06-05 11:59:27 +0900

    お知らせマスタの修正

--- Data/Master/NewsMB.cs
+++ Data/Master/NewsMB.cs
@@ -4,35 +4,41 @@ using PM.Enum.News;
 using PM.Enum.Notification;
 
 [Description("NewsMB")]
-public class NewsMB : MasterBookBase {
-    /// <summary>
-    /// タイプ
-    /// </summary>
-    public NewsType type { get; set; }
+public class NewsMB : MasterBookBase
+{
+  /// <summary>
+  /// タイプ
+  /// </summary>
+  public NewsType type { get; set; }
 
-    /// <summary>
-    /// タイトル
-    /// </summary>
-    public string title { get; set; }
+  /// <summary>
+  /// タイトル
+  /// </summary>
+  public string title { get; set; }
 
-    /// <summary>
-    /// 条件リスト
-    /// 条件を満たすときだけ処理実行
-    /// </summary>
-    public List<ConditionMI> conditionList { get; set; }
+  /// <summary>
+  /// 条件リスト
+  /// 条件を満たすときだけ処理実行
+  /// </summary>
+  public List<ConditionMI> conditionList { get; set; }
 
-    /// <summary>
-    /// 繰り返しタイプ
-    /// </summary>
-    public RepeatType repeatType { get; set; }
+  /// <summary>
+  /// 繰り返しタイプ
+  /// </summary>
+  public RepeatType repeatType { get; set; }
 
-    /// <summary>
-    /// メッセージ
-    /// </summary>
-    public string message { get; set; }
+  /// <summary>
+  /// メッセージ
+  /// </summary>
+  public string message { get; set; }
 
-    /// <summary>
-    /// アイテムリスト
-    /// </summary>
-    public List<ItemMI> itemList { get; set; }
+  /// <summary>
+  /// プレゼント配布時のプレゼント画面での表示文言メッセージ
+  /// </summary>
+  public string presentMessage { get; set; }
+
+  /// <summary>
+  /// アイテムリスト
+  /// </summary>
+  public List<ItemMI> itemList { get; set; }
 }
